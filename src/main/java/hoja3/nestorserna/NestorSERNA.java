8cdb5efb3942e6a17c5cd5dd6b7f38167e0f9e0d
nestor.sernavaquero@iesmiguelherrero.com <Usuario@DESKTOP-6OP7UMD>
2024-06-20 23:20:47 +0200

    cambios main12

--- src/main/java/hoja3/nestorserna/NestorSERNA.java
+++ src/main/java/hoja3/nestorserna/NestorSERNA.java
@@ -13,6 +13,7 @@ public class NestorSERNA {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("adasd");
+        System.out.println("adsdasda");
     }
     
     
